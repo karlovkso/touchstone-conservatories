05752821aca982aecb6b9613fe8f227e3d140061
Vinceee-ops <minarimyoui106@gmail.com>
2024-12-03 13:50:23 +0800

    About and Feedback page

--- touchstone-conservatories/src/Pages/Main.jsx
+++ touchstone-conservatories/src/Pages/Main.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React from 'react';
 
 export default function Main() {
   return (
-    <div className='Main' id='Home'>Main</div>
-  )
+    <section id="Main" className="py-20 bg-gray-100">
+      <h2 className="text-4xl text-center font-bold text-black">Main Section</h2>
+      <p className="text-center mt-4 text-lg">Content for the Main section...</p>
+    </section>
+  );
 }

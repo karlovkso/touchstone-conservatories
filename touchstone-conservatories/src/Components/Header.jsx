7aeea78a09199e279fb31a434b1dfe3e73e450d7
ALO <135142990+karlovkso@users.noreply.github.com>
2024-12-03 12:10:04 +0800

    Header, Main, and Welcome Layers

--- touchstone-conservatories/src/Components/Header.jsx
+++ touchstone-conservatories/src/Components/Header.jsx
@@ -1,40 +1,90 @@
-import React, { useState } from 'react'
-import { Link } from 'react-scroll'
+import React, { useState } from "react";
+import { Link } from "react-scroll";
+import logo from "../assets/logo.png";
 
 function Header() {
-const [click, setClick] = useState(false)
-    const handleClick = () => setClick(!click)
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+  const closeMenu = () => setClick(false);
 
-    const closeMenu = () => setClick(false)
+  return (
+    <div className="header" id="Header">
+      <nav className="navbar justify-between px-14 py-6">
+        <a href="/" className="logo">
+          <img src={logo} alt="logo" width="300" />
+        </a>
 
-    return (
-        <div className='header'>
-            <nav className='navbar'>
-                {/* <a href='/' className='logo'>
-                    <img src={logo} alt='logo' />
-                </a> */}
-                {/* <div className='hamburger' onClick={handleClick}>
+        {/* <div className='hamburger' onClick={handleClick}>
                     {click ? (<FaTimes size={30} style={{ color: '#ffffff' }} />)
                         : (<FaBars size={30} style={{ color: '#ffffff' }} />)}
 
                 </div> */}
-                <ul className={click ? "nav-menu active" : "nav-menu"}>
-                    <li className='nav-item'>
-                        <Link to='Home' spy={true} smooth={true} offset={-100} duration={500} onClick={closeMenu}>Home</Link>
-                    </li>
-                    <li className='nav-item'>
-                        <Link to='Welcome' spy={true} smooth={true} offset={-100} duration={500} onClick={closeMenu}>Welcome</Link>
-                    </li>
-                    <li className='nav-item'>
-                    <Link to='About' spy={true} smooth={true} offset={-100} duration={500} onClick={closeMenu}>About</Link>
-                    </li>
-                    <li className='nav-item'>
-                    <Link to='Feedback' spy={true} smooth={true} offset={-100} duration={500} onClick={closeMenu}>Feedback</Link>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-    )
+
+        <ul
+          className={`nav-menu ${
+            click ? "active" : ""
+          } space-x-10 text-lg text-cyan-700`}
+        >
+          <li className="nav-item hover:font-bold">
+            <button>
+              <Link
+                to="Header"
+                spy={true}
+                smooth={true}
+                offset={-100}
+                duration={500}
+                onClick={closeMenu}
+              >
+                Home
+              </Link>
+            </button>
+          </li>
+          <li className="nav-item hover:font-bold">
+            <button>
+              <Link
+                to="Welcome"
+                spy={true}
+                smooth={true}
+                offset={-100}
+                duration={500}
+                onClick={closeMenu}
+              >
+                Welcome
+              </Link>
+            </button>
+          </li>
+          <li className="nav-item hover:font-bold">
+            <button>
+              <Link
+                to="About"
+                spy={true}
+                smooth={true}
+                offset={-100}
+                duration={500}
+                onClick={closeMenu}
+              >
+                About
+              </Link>
+            </button>
+          </li>
+          <li className="nav-item hover:font-bold">
+            <button>
+              <Link
+                to="Feedback"
+                spy={true}
+                smooth={true}
+                offset={-100}
+                duration={500}
+                onClick={closeMenu}
+              >
+                Feedback
+              </Link>
+            </button>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
--- touchstone-conservatories/src/Pages/Main.jsx
+++ touchstone-conservatories/src/Pages/Main.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React from "react";
+import background from "../assets/background.png";
 
 export default function Main() {
   return (
-    <div className='Main' id='Home'>Main</div>
-  )
+    <div className="Main" id="Home">
+      <div
+        className="hero min-h-screen bg-cyan-700"
+        style={{
+          backgroundImage: `url(${background})`,
+        }}
+      >
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-neutral-content text-center">
+          <div className="max-w-full text-white">
+            <h1 className="mb-5 text-8xl font-bold">
+              We Create Space, Handcrafted For Connection
+            </h1>
+            <p className="mt-10 mb-5 text-2xl">
+              We build elegant, custom conservatories that transform your home
+              and outdoor spaces. Our designs bring natural light, comfort, and
+              style to your life. Ready to create your dream space? Contact us
+              today for a free consultation!
+            </p>
+            <button className="btn text-sm mt-5 text-black bg-white uppercase border-transparent hover:border-transparent hover:bg-cyan-700 hover:text-white">
+              Get Free Design
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
